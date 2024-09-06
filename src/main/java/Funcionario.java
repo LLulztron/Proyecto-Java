9814ca65b129cb381dca1c42bf306a25d2c3910e
LLulztron <163662385+LLulztron@users.noreply.github.com>
2024-09-06 13:16:30 +0000

    TERMINO de clase funcionario

--- src/main/java/Funcionario.java
+++ src/main/java/Funcionario.java
@@ -10,8 +10,8 @@ public class Funcionario
     }
 
     public String getNombre() { return nombre; }
-
     public String getPuesto() { return puesto; }
 
-    
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setPuesto(String puesto) { this.puesto = puesto; }
 }
